88b160d8e3fd505c0e40fed56513765e5ff54ea9
ForsakeLJK <forsakeljk@gmail.com>
2020-06-01 23:18:35 +0800

    knn implemented

--- knn/knn.py
+++ knn/knn.py
@@ -16,10 +16,24 @@ def knn(x, x_train, y_train, k):
 
     # Warning: uint8 matrix multiply uint8 matrix may cause overflow, take care
     # Hint: You may find numpy.argsort & scipy.stats.mode helpful
-	#TODO
     # YOUR CODE HERE
-
+    
     # begin answer
+    
+    P = x_train.shape[1]
+    N = x_train.shape[0]
+    N_test = x.shape[0]
+    y = np.zeros((N_test, ))
+
+    for i in range(N_test):
+        tmp1 = np.abs(x_train - x[i, :])
+        tmp2 = np.square(tmp1)
+        # shape (N, )
+        distances = np.sum(tmp2, axis=1)
+        # find k nearest neighbors, shape (k, )
+        nearestNeighborsIndices = np.argsort(distances)[:k]
+        y[i] = np.argmax(np.bincount(y_train[nearestNeighborsIndices]))
+
     # end answer
 
     return y
